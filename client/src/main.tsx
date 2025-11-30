27b621210596093b2ab101e8a44a2d585a6a147f
sumitkumards07 <sumitkumards07@gmail.com>
2025-11-30 17:38:50 +0530

    Force frontend rebuild with version log

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,4 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+console.log("App Version: 2025-11-30 17:40 - Force Update");
+
 createRoot(document.getElementById("root")!).render(<App />);
