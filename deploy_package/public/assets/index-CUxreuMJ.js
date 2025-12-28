55ade01ff4a5368ef10eb50050b96c4673ccb184
sumitkumards07 <sumitkumards07@gmail.com>
2025-12-28 21:38:30 +0530

    chore: remove compromised build artifacts and ignore deploy_package

--- deploy_package/public/assets/index-CUxreuMJ.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CyXqPxeb.js","assets/index-Cn8nHwTB.js","assets/index-wcNd357R.css"])))=>i.map(i=>d[i]);
-import{r,_ as t}from"./index-Cn8nHwTB.js";const o=r("Share",{web:()=>t(()=>import("./web-CyXqPxeb.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ShareWeb)});export{o as Share};
